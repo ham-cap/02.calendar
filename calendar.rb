a1f86cef9edc93ea073fa2391845cb062e9d352e
Toshiyasu Nonohara <hh0648075@gmail.com>
2020-12-17 05:52:31 +0900

    初日の曜日を判定して先頭の空白文字数を調整

---
+++ calendar.rb
@@ -0,0 +1,48 @@
+require 'date'
+require 'optparse'
+
+options = ARGV.getopts('m:y:')
+year = options["y"]
+month = options["m"]
+
+if year && month
+  puts "　　　　#{month}月　#{year}"
+  puts "日　月　火　水　木　金　土"
+else
+  today = Date.today.to_s.split("-")
+  this_year = today[0]
+  this_month = today[1]
+  this_day = today[2]
+  puts "　　　　#{this_month}月 #{this_year}"
+  puts "日　月　火　水　木　金　土"
+end
+
+first_day = Date.parse("#{year}-#{month}-1")
+final_day = Date.new(year.to_i, month.to_i, -1)
+days = (first_day..final_day)
+days_array = []
+if first_day.monday?
+  days_array.push("　　#{first_day.day}")
+elsif first_day.tuesday?
+  days_array.push("　　　　#{first_day.day}")
+elsif first_day.wednesday?
+  days_array.push("　　　　　　#{first_day.day}")
+elsif first_day.thursday?
+  days_array.push("　　　　　　　　#{first_day.day}")
+elsif first_day.friday?
+  days_array.push("　　　　　　　　　　#{first_day.day}")
+elsif first_day.saturday?
+  days_array.push("　　　　　　　　　　　　#{first_day.day}\n")
+else first_day.sunday?
+  days_array.push("#{first_day.day}")
+end
+puts days_array
+  
+end
+
+# 曜日の判定処理
+# if day.saturday?
+#   print "#{day.day}\n"
+# else
+#   print "#{day.day}　"
+# end
